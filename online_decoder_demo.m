385a6b0cacda14dd74bd8f2e5d1f35fcd68e3693
WeiHsien Lee/Willy <42806161+fffairforce@users.noreply.github.com>
2021-12-09 15:22:50 -0600

    Update online_decoder_demo.m

--- online_decoder_demo.m
+++ online_decoder_demo.m
@@ -1,5 +1,12 @@
-% online decoder1: 
+%% online decoder1: Base KF
 % take in SBP
+% SpikingBandPower: The spiking band power for all 96 channels accumulated each ms, acquired via the following procedure:
+% 			1) Filtered to 300-1,000Hz using the Digital Filter Editor in the Central Software Suite (Blackrock Microsystems, LLC).
+% 			2) Sampled at 2 kilo-samples per second.
+% 			3) Transmitted from the Cerebus system to the xPC Target system (see methods in the main paper) at 2 kilo-samples per second.
+% 			4) All samples received by the xPC within 1ms are absolute-valued then summed within each channel, contained in the SpikingBandPower field.
+
+% use simulink for data intake?
 
 % initialize-from create kalman
 % set initial states
@@ -11,10 +18,10 @@ Pj=P_0;
 %VELOCITY OUTPUT
 %store previous state Xt, the position and velocity
 %at time t is used to calculate the position at time t+1
-%ch--channels chosen to decode
+
 %state_dim--if task set to different states(e.g. finger
 %movements)/potentially edit
-ch=N;
+ch=N; %ch--channels chosen to decode
 Xtprev = Xt;
 Xtoutprev = Xtout;
 % Normal Update
@@ -29,7 +36,12 @@ Xt = At*Xt;                 % prediction from previous state
 prior_P = At*Pj*At'+Wt;
 
 % Innovate/Update:
-%Kt = Pj*C'/(C*Pj*C' + Q);
-Kt = (eye(size(prior_P))+prior_P*CtQinvCT)\prior_P*CtQinvT;
-Xt = Xt + Kt*(double(lags(chans,binLag+1)) - Ct*Xt);      % correct prediction
+Kt = Pj*C'/(C*Pj*C' + Q);
+%Kt = (eye(size(prior_P))+prior_P*CtQinvCT)\prior_P*CtQinvT;
+Y_error = Y(:,t) - C*predX(:,t-1);
+Xt = Xt + Kt*Y_error;      % correct prediction   (double(lags(chans,binLag+1)) - Ct*Xt)
 Pj = (eye(size(prior_P)) - Kt*Ct)*prior_P;                        % Update movement covariance matrix
+
+%current state position = position + velocity*dt 
+%% online decoder2:Multistate Decoder
+%assume input with three class SBP
