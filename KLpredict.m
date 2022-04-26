9ea911dab3672a3b6a6d487304b006f1e50fd8fa
WeiHsien Lee/Willy <42806161+fffairforce@users.noreply.github.com>
2022-04-26 00:41:16 -0500

    update

--- KLpredict.m
+++ KLpredict.m
@@ -1,7 +1,7 @@
 function predXX = KLpredict(XX,YY,dt)
 [A,C,Q,W,P_0] = train_online_kalman(XX(1:end-1,:),YY,dt);
 predXX = zeros(size(A,1), size(YY,2));
-X_0(1:end-1,1) = XX(1:end-1,1);
+X_0 = XX(1:end-1,1);
 P=P_0;
 predXX(:,1) = X_0;
 K = zeros(size(W,1), size(Q,1), size(YY,2));
